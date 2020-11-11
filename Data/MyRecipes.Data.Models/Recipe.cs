690f18b498b81a0fc86b033db226972808fa0e8d
plus1pxl <plus1pxl@users.noreply.github.com>
2020-11-11 21:16:13 +0200

    InitialCreate

---
+++ Data/MyRecipes.Data.Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿namespace MyRecipes.Data.Models
+{
+    using System;
+    using MyRecipes.Data.Common.Models;
+
+    public class Recipe : BaseDeletableModel<int>
+    {
+        public TimeSpan PreparationTime { get; set; }
+
+        public TimeSpan CookingTime { get; set; }
+
+        public int PortionCount { get; set; }
+    }
+}
