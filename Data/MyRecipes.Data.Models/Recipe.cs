dc71c220a0405f07f64cfc4bed8ec5953026601a
plus1pxl <plus1pxl@users.noreply.github.com>
2020-11-12 11:25:41 +0200

    Add Models

--- Data/MyRecipes.Data.Models/Recipe.cs
+++ Data/MyRecipes.Data.Models/Recipe.cs
@@ -1,14 +1,31 @@
-﻿namespace MyRecipes.Data.Models
+﻿using System.Collections.Generic;
+
+namespace MyRecipes.Data.Models
 {
     using System;
     using MyRecipes.Data.Common.Models;
 
     public class Recipe : BaseDeletableModel<int>
     {
+
+        public string Name { get; set; }
+
+        public string Instructions { get; set; }
+
         public TimeSpan PreparationTime { get; set; }
 
         public TimeSpan CookingTime { get; set; }
 
         public int PortionCount { get; set; }
+
+        public string AddedByUserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<RecipeIngredient> Ingredients { get; set; } = new HashSet<RecipeIngredient>();
     }
 }
